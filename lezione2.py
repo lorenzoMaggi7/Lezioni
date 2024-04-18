cbd7ae89239c893a70517221c1cc114c065bfce4
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2024-04-18 13:54:43 +0200

    esercizio 3-7 risolto

--- lezione2.py
+++ lezione2.py
@@ -89,4 +89,35 @@ people.insert(2, "Micheal Jordan")
 people.append("Antonio Cassano")
 for i in people:
     print(f"Hi {i},tonight we are gonna have fun")
+print("\n")
+
+
+"""
+3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and now you have space for only two guests.
+• Start with your program from Exercise 3-6. Add a new line that prints a message saying that you can invite only two people for dinner.
+• Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list,
+ print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+• Print a message to each of the two people still on your list, letting them know they’re still invited.
+• Use del to remove the last two names from your list, so you have an empty list.
+Print your list to make sure you actually have an empty list at the end of your program.
+"""
+
+print(f"Guys, i'm sorry but i only can invite just 2 people tonight")
+print(f"{people[0]}, lo siento pero no puedo tenerte esta noche")
+people.pop(0)
+print(f"{people[0]}, lo siento pero no puedo tenerte esta noche")
+people.pop(0)
+print(f"{people[0]}, i'm sorry but i can't have you tonight")
+people.pop(0)
+print(f"{people[0]}, i'm sorry but i can't have you tonight")
+people.pop(0)
+print(f"{people[0]}, sta sera ce stamo io te e antonio, se divertimo")
+print(f"{people[1]}, sta sera ce stamo io te e francesco, se divertimo")
+print("\n")
+del people[0]
+del people[0]
+print(people)
+
+
+
 
