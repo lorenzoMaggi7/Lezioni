51f4910b753f08a3bcb7ec3ad5c6eb781153de88
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2024-04-18 12:57:16 +0200

    esercizio 3-3 risolto

--- lezione2.py
+++ lezione2.py
@@ -43,3 +43,10 @@ print(names[2])
 print(f"Ciao {names[0]} come stai?")
 print(f"Ciao {names[1]} come stai?")
 print(f"Ciao {names[2]} come stai?")
+
+#3-3. Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
+#Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+cars:list =("Porsche 911", "Toyota Supra", "Ferrari Roma")
+print(f"I would like to own a {cars[0]}")
+print(f"I would like to buy a {cars[1]}") 
+print(f"I would like to drive a {cars[2]}")
