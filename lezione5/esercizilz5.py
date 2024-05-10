818f3ea9fd0cbbbb0bd204d9d60e5e91bff6ba39
Nio4124 <antoniozottola41204@gmail.com>
2024-05-10 19:16:49 +0200

    9-14 fatto

--- lezione5/esercizilz5.py
+++ lezione5/esercizilz5.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Food:
 
     
@@ -285,3 +288,86 @@ class Admin(User):
 
 admin_privileges = Admin("Lorenzo","Maggi",22,1.80,["can add post", "can delete post", "can ban user", "can promote user", "can delete all"])
 admin_privileges.show_priviliges()
+
+
+"""
+9-8. Privileges: Write a separate Privileges class. The class should have one attribute, privileges, that stores a list of strings as described in Exercise 9-7. 
+Move the show_privileges() method to this class. Make a Privileges instance as an attribute in the Admin class. 
+Create a new instance of Admin and use your method to show its privileges.
+"""
+class Privileges:
+    def __init__(self, privileges: list[str]):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print("Here are your privileges:")
+        for i in self.privileges:
+            print(i)
+
+class Admin(User):
+    def __init__(self, first_name: str, last_name: str, age: int, height: float, privileges: list[str]):
+        super().__init__(first_name, last_name, age, height)
+        self.privileges = Privileges(privileges)
+
+admin_privileges = Admin("Oussama", "Hliwa", 22, 176, ["can add post", "can delete post", "can ban user", "can promote user", "can delete all"])
+admin_privileges.privileges.show_privileges()
+
+
+"""
+9-9. Battery Upgrade: Use the final version of electric_car.py from this section. 
+Add a method to the Battery class called upgrade_battery(). This method should check the battery size and set the capacity to 65 if it isn’t already. 
+Make an electric car with a default battery size, call get_range() once, and then call get_range() a second time after upgrading the battery. 
+You should see an increase in the car’s range.
+"""
+
+
+
+"""
+9-10. Imported Restaurant: Using your latest Restaurant class, store it in a module. Make a separate file that imports Restaurant. 
+Make a Restaurant instance, and call one of Restaurant’s methods to show that the import statement is working properly.
+"""
+
+"""
+9-11. Imported Admin: Start with your work from Exercise 9-8. Store the classes User, Privileges, and Admin in one module. 
+Create a separate file, make an Admin instance, and call show_privileges() to show that everything is working correctly.
+"""
+
+"""
+9-12. Multiple Modules: Store the User class in one module, and store the Privileges and Admin classes in a separate module. 
+In a separate file, create an Admin instance and call show_privileges() to show that everything is still working correctly.
+"""
+
+
+"""
+9-13. Dice: Make a class Die with one attribute called sides, which has a default value of 6. 
+Write a method called roll_die() that prints a random number between 1 and the number of sides the die has. 
+Make a 6-sided die and roll it 10 times. Make a 10-sided die and a 20-sided die. Roll each die 10 times.
+"""
+class Dice:
+    def __init__(self, sides:int = 6):
+        self.sides = sides
+
+    def roll_dice(self):
+        print(f"You're rolling a {self.sides} dice")
+        for _ in range(10):
+            print(random.randint(1, self.sides))
+
+six_sided_die = Dice()
+six_sided_die.roll_dice()
+
+ten_sided_die = Dice(sides=10)
+ten_sided_die.roll_dice()
+
+twenty_sided_die = Dice(sides=20)
+twenty_sided_die.roll_dice()
+
+"""
+9-14. Lottery: Make a list or tuple containing a series of 10 numbers and 5 letters. 
+Randomly select 4 numbers or letters from the list and print a message saying that any ticket matching these 4 numbers or letters wins a prize.
+"""
+lottery_numbers_and_letters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'B', 'C', 'D', 'E']
+
+selected_items = random.sample(lottery_numbers_and_letters, 4)
+
+print("The winning combination is:", selected_items)
+print("Any ticket matching these 4 numbers or letters wins a prize!")
